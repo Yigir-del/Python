506cd506ebdd3c0712efb45f37e621bc8df5d392
Yigir-del <altuntasy83@gmail.com>
2025-02-01 16:38:10 +0300

    Update problemler.py

--- problemler.py
+++ problemler.py
@@ -7,3 +7,19 @@ if sumlist == int(x):
     print(f"{x} sayısı bir armstrong sayısıdır.")
 else:
     print(f"{x} sayısı bir armstrong sayısı değildir. {sumlist}")
+
+
+
+print("Çarpım Tablosu Programı\n")
+
+for i in range(1, 11):  # 1'den 10'a kadar
+    for k in range(1, 6):  # İlk 5 sütun
+        print(f"{k} x {i} = {i*k}".ljust(12), end=" ")  # Sabit genişlikte hizala
+    print()  # Her satırdan sonra alt satıra geç
+
+print()  # İki tabloyu ayırmak için boşluk
+
+for i in range(1, 11):  # 1'den 10'a kadar
+    for k in range(6, 11):  # 6-10 arasındaki sütunlar
+        print(f"{k} x {i} = {i*k}".ljust(12), end=" ")  
+    print()  
