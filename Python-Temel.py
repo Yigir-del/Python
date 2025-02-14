9bb2169c81b400d708a4fa0c9157fe1cd2f04d07
Yigir-del <altuntasy83@gmail.com>
2025-02-15 00:59:02 +0300

    Update Python-Temel.py

--- Python-Temel.py
+++ Python-Temel.py
@@ -79,55 +79,5 @@ print("Oyuncunun adi:{} \n Oyuncunun soyadi:{} \n oyuncunun oynadiği takim:{} "
 
 
 
-#Açıklamaya üşeniyorum kendin çöz ne iş yaptıklarını 
-num = 100
-print(num)
-print(bin(num))
-print(hex(num))
-
-num1 = -5
-print(abs(num1))  #mutlak deger
-
-print(round(3.5))
-print(round(3.6))
-print(round(3.4))
-
-print(max(3,4,5,6,-2,34))
-print(min(3,4,5,6,-2,34))
-
-print(sum([3,4,5,1,4,5]))
-
-print(pow(2,4)) #2**4
-
-
-
-#Açıklamaya üşendiğim string ve dizi methodları.
-string1 = "pyTHon"
-print(string1)
-print(string1.upper())
-print(string1.lower())
-
-string2 = "Herkes ana baba baci gardas"
-print(string2)
-print(string2.replace("a","o"))
-print(string2.replace(" ","-"))
-
-string3 = "Adana Demir Spor"
-print(string3.startswith("Ad"))
-print(string3.endswith("or"))
-
-string4 = "Python programlama dili"
-print(string4.split(" "))
-print(string4.strip("P"))  #aradığın başta veya sondaysa siler
-print(string4.lstrip("i")) #aradığın sondaysa siler
-print(string4.rstrip("i")) #aradığın baştaysa siler
-print(string4.count(" "))
-print(string4.find("o"))
-print(string4.find("z"))
-print(string4.rfind("o"))
-
-dizi = ["27","01","2005"]
-print("/".join(dizi))
-
 
 
